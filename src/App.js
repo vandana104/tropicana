6fa07a3fff982e32b74f186dcd94a871a165a7ce
vandana104 <vandanaoel@gmail.com>
2023-12-09 17:23:53 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import Dropdown from "./Components/Dropdown";
+import Authentication from "./Components/Authentication";
 import Footer from "./Components/Footer";
 import HomePage from "./Components/HomePage";
 import Navbar from "./Components/Navbar";
-import Section2 from "./Components/Section2";
-import SignUp from "./Components/SignUp";
-import Verification from "./Components/Verification";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="relative">
       <Navbar />
-      {/* <Section2/> */}
-      
-     
+
       <Routes>
         <Route path="/" exact element={<HomePage />} />
-        <Route path="/signup" exact element={<SignUp />} />
-        <Route path="/verification" exact element={<Verification />} />
+        <Route path="/signUp" element={<Authentication />} />
+        <Route path="/login" element={<Authentication />} />
+        <Route path="/verify" element={<Authentication />} />
       </Routes>
       <Footer />
     </div>
